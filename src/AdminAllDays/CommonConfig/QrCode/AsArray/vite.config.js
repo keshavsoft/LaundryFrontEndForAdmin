edd4765771d2ee78ef36facbbc6be4454a23328d
keshavsoft <nknnkumar@yahoo.com>
2024-11-14 19:41:04 +0530

    still not working

--- src/AdminAllDays/CommonConfig/QrCode/AsArray/vite.config.js
+++ src/AdminAllDays/CommonConfig/QrCode/AsArray/vite.config.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const SourceFolderName = "SourceCode";
-const SrcFolder = "src/Laundry/Admin/AdminAllDays/CommonConfig/QrCode/AsArray";
+const SrcFolder = "src/AdminAllDays/CommonConfig/QrCode/AsArray";
 
 const FrontEndDistFolder = "publicDir/Admin/AdminAllDays/CommonConfig/QrCode/AsArray";
 
