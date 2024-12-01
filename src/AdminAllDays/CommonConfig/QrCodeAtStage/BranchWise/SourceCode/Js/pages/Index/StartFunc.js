c5cd3532068619bf1ff5789dd9598a3f83fb0c96
keshavsoft <nknnkumar@yahoo.com>
2024-12-01 17:48:06 +0530

    card started

--- src/AdminAllDays/CommonConfig/QrCodeAtStage/BranchWise/SourceCode/Js/pages/Index/StartFunc.js
+++ src/AdminAllDays/CommonConfig/QrCodeAtStage/BranchWise/SourceCode/Js/pages/Index/StartFunc.js
@@ -10,23 +10,33 @@ const jFLocalLeftSide = () => {
     document.querySelector(".revenue-card").querySelector("h6").innerHTML = "16";
 };
 
-// Test to see if the browser supports the HTML template element by checking
-// for the presence of the template element's content attribute.
+const jVarLocalData = [
+    {
+        BranchName: "KKD"
+    },
+    {
+        BranchName: "VSKP"
+    },
+    {
+        BranchName: "Wanaparthy"
+    }
+];
+
+
 if ("content" in document.createElement("template")) {
-    // Instantiate the table with the existing HTML tbody
-    // and the row with the template
     const tbody = document.querySelector("#MainRowId");
-    const template = document.querySelector("#CardForTotalQrCodesId");
 
-    // Clone the new row and insert it into the table
-    const clone = template.content.cloneNode(true);
-    let td = clone.querySelector("h6");
-    td.innerHTML = "1235646565";
+    jVarLocalData.forEach(element => {
+        const template = document.querySelector("#TemplateForCardId");
 
-    tbody.appendChild(clone);
+        const clone = template.content.cloneNode(true);
+        let td = clone.querySelector(".card-body h6");
+        td.innerHTML = element.BranchName;
+
+        tbody.appendChild(clone);
+    });
+
 } else {
-    // Find another way to add the rows to the table because
-    // the HTML template element is not supported.
 };
 
 StartFunc();
