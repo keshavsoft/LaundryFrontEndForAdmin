cb3107b60e2becfc8d59d78ab4046c25cd3d5054
keshavsoft <nknnkumar@yahoo.com>
2024-12-01 13:26:05 +0530

    dashboard card started

--- src/AdminAllDays/CommonConfig/QrCodeAtStage/BranchWise/SourceCode/Js/pages/Index/StartFunc.js
+++ src/AdminAllDays/CommonConfig/QrCodeAtStage/BranchWise/SourceCode/Js/pages/Index/StartFunc.js
@@ -15,27 +15,18 @@ const jFLocalLeftSide = () => {
 if ("content" in document.createElement("template")) {
     // Instantiate the table with the existing HTML tbody
     // and the row with the template
-    const tbody = document.querySelector("tbody");
+    const tbody = document.querySelector("#MainRowId");
     const template = document.querySelector("#CardForTotalQrCodesId");
 
     // Clone the new row and insert it into the table
     const clone = template.content.cloneNode(true);
-    let td = clone.querySelector(".h6");
+    let td = clone.querySelector("h6");
     td.innerHTML = "1235646565";
 
     tbody.appendChild(clone);
-
-    // Clone the new row and insert it into the table
-    const clone2 = template.content.cloneNode(true);
-    td = clone2.querySelectorAll("td");
-    td[0].textContent = "0384928528";
-    td[1].textContent = "Acme Kidney Beans 2";
-
-    tbody.appendChild(clone2);
 } else {
     // Find another way to add the rows to the table because
     // the HTML template element is not supported.
-}
-
+};
 
 StartFunc();
