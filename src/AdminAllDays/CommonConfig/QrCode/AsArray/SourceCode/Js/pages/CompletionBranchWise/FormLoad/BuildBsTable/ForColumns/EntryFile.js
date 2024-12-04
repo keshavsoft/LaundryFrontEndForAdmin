b523ea0ba5b8c67ae8f5e9a169c10d78f195bfe4
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2024-12-04 12:12:38 +0530

    Status Updated

--- src/AdminAllDays/CommonConfig/QrCode/AsArray/SourceCode/Js/pages/CompletionBranchWise/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/AdminAllDays/CommonConfig/QrCode/AsArray/SourceCode/Js/pages/CompletionBranchWise/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,11 +1,13 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+import { StartFunc as ColumnStatus } from "./ColumnStatus/entryFile.js";
 
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalColumnStatus = LocalColumns.find(element => element.field === "CompletionScan");
 
 
     if (LocalColumnOperateFine === undefined === false) {
@@ -13,6 +15,9 @@ let StartFunc = ({ inColumns }) => {
     };
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
+    };
+    if (LocalColumnStatus === undefined === false) {
+        ColumnStatus({ inFindColumn: LocalColumnStatus });
     };
 };
 
