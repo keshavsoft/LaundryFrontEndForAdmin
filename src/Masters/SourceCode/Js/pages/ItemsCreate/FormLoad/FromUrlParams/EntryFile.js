e90e96f3b2abdbe579024caba220b6b00cd08745
Mounika Vanamala <mounicavanamala@gmail.com>
2024-12-23 15:12:28 +0530

    changes in columns

--- src/Masters/SourceCode/Js/pages/ItemsCreate/FormLoad/FromUrlParams/EntryFile.js
+++ src/Masters/SourceCode/Js/pages/ItemsCreate/FormLoad/FromUrlParams/EntryFile.js
@@ -1,7 +1,7 @@
 let StartFunc = () => {
     let jVarLocalBranchId = 'BranchNameId';
     let jVarlocalBranch = document.getElementById(jVarLocalBranchId);
-    let jVarLocalBranchName = localStorage.getItem("BranchName");
+    let jVarLocalBranchName = localStorage.getItem("Branch Name");
 
     if (jVarlocalBranch === null === false) {
         jVarlocalBranch.innerHTML = jVarLocalBranchName;
