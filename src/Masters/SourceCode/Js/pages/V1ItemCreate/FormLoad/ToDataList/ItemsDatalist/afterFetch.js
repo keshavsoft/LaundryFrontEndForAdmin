ef21439bb4267c67e1396adb5d2205f027eaefb9
pushpadamalanka82 <y*********@gmail.com>
2024-12-27 13:53:07 +0530

    changes

--- src/Masters/SourceCode/Js/pages/V1ItemCreate/FormLoad/ToDataList/ItemsDatalist/afterFetch.js
+++ src/Masters/SourceCode/Js/pages/V1ItemCreate/FormLoad/ToDataList/ItemsDatalist/afterFetch.js
@@ -2,22 +2,13 @@ const StartFunc = (data) => {
 
     data.forEach(element => {
         jFLocalAddToItems({ inItemName: element.ItemName });
-        jFLocalItemService({ inItemService: element.ItemService });
-
     });
-
 };
 
 const jFLocalAddToItems = ({ inItemName }) => {
-    var option = $('<option value="' + inItemName + '"></option>');
-
+    const option = `<option value="${inItemName}">${inItemName}</option>`
+    // var option = $('<option value="' + inItemName + '"></option>');
     $('#ItemNameDataListId').append(option);
-};
-
-const jFLocalItemService = ({ inItemService }) => {
-    var option = $('<option value="' + inItemService + '"></option>');
-
-    $('#ItemServiceDataListId').append(option);
 };
 
 export { StartFunc };
