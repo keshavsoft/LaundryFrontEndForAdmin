55e6ecf5b376b400e2ee7b721e5590c5468d59c6
pushpadamalanka82 <y*********@gmail.com>
2024-12-30 16:19:07 +0530

    returnforfactory

---
+++ src/AdminAllDays/CommonConfig/QrCode/AsArray/SourceCode/Js/pages/FactoryScanReturn/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -0,0 +1,27 @@
+import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+import { StartFunc as ColumnStatus } from "./ColumnStatus/entryFile.js";
+import { StartFunc as ColumnReturn } from "./ColumnReturn/entryFile.js";
+
+let StartFunc = ({ inColumns }) => {
+    let LocalColumns = inColumns;
+    let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
+    let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalColumnStatus = LocalColumns.find(element => element.field === "FactoryScan");
+    let LocalColumnReturn = LocalColumns.find(element => element.field === "EntryReturnScan");
+
+    if (LocalColumnOperateFine === undefined === false) {
+        ColumnOperate({ inFindColumn: LocalColumnOperateFine });
+    };
+    if (LocalColumnRate === undefined === false) {
+        ColumnRate({ inFindColumn: LocalColumnRate });
+    };
+    if (LocalColumnStatus === undefined === false) {
+        ColumnStatus({ inFindColumn: LocalColumnStatus });
+    };
+    if (LocalColumnReturn === undefined === false) {
+        ColumnReturn({ inFindColumn: LocalColumnReturn });
+    };
+};
+
+export { StartFunc };
