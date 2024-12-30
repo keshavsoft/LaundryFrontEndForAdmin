3f9d4bbf79264cb76672e66007e561e67496398b
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2024-12-30 19:39:27 +0530

    rewash, return and filter is updated

--- src/AdminAllDays/CommonConfig/QrCode/BranchFilter/SourceCode/Js/pages/PressingReturnRewash/FormLoad/BuildBsTable/ForColumns/ColumnRewash/entryFile.js
+++ src/AdminAllDays/CommonConfig/QrCode/BranchFilter/SourceCode/Js/pages/PressingReturnRewash/FormLoad/BuildBsTable/ForColumns/ColumnRewash/entryFile.js
@@ -5,9 +5,9 @@ let StartFunc = ({ inFindColumn }) => {
 let jVarLocalFormatterFunc = (value, row, index) => {
     console.log("value : ", value);
     if (value === true) {
-        return '<span class="badge bg-success"> ✔</span>';
+        return '<span class="badge bg-success">True</span>';
     } else {
-        return '<span class="badge bg-danger">✘</span>';
+        return '<span class="badge bg-danger">False</span>';
     };
 };
 
