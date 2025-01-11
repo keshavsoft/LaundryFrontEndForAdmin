9f16daff45530de6564233e11a48144d07e6d6f4
sharath0036 <ksharathbabu2@gmail.com>
2025-01-11 12:45:56 +0530

    bugs fixed

--- src/Masters/SourceCode/Js/pages/Index/FormLoad/ShowOnDom/MastersItemTypes/GetFetch.js
+++ src/Masters/SourceCode/Js/pages/Index/FormLoad/ShowOnDom/MastersItemTypes/GetFetch.js
@@ -2,7 +2,7 @@ import UrlJson from "../../../Config.json" with { type: "json" };
 
 let StartFunc = async () => {
     let LocalroutePath = UrlJson.routePath;
-    let LocalMastersItemstableName = UrlJson.MastersItemstableName;
+    let LocalMastersItemstableName = UrlJson.MastersItemTypestableName;
 
     let jVarLocalFetchUrl = `/${LocalroutePath}/${LocalMastersItemstableName}/AggrFuncs/Count`;
     let response = await fetch(jVarLocalFetchUrl);
