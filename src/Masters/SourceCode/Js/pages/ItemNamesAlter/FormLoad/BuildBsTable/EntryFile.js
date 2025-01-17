84f163d2c0ae8410d207c40aa6b533963120bfc7
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2025-01-17 19:06:10 +0530

    alter and delete button in customer and item

---
+++ src/Masters/SourceCode/Js/pages/ItemNamesAlter/FormLoad/BuildBsTable/EntryFile.js
@@ -0,0 +1,11 @@
+import optionsJson from './options.json' with {type: 'json'};
+import { StartFunc as StartFuncForColumns } from "./ForColumns/EntryFile.js";
+
+const StartFunc = () => {
+    var $table = $('#table');
+    StartFuncForColumns({ inColumns: optionsJson.columns })
+    $table.bootstrapTable(optionsJson);
+};
+
+export { StartFunc };
+
