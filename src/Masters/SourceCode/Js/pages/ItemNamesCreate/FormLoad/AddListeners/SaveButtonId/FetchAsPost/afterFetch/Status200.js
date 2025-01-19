4a2efc39e301fe0cc70957c781c28b7c24ef97d2
keshavsoft <nknnkumar@yahoo.com>
2025-01-19 21:39:49 +0530

    itemnames create working good

--- src/Masters/SourceCode/Js/pages/ItemNamesCreate/FormLoad/AddListeners/SaveButtonId/FetchAsPost/afterFetch/Status200.js
+++ src/Masters/SourceCode/Js/pages/ItemNamesCreate/FormLoad/AddListeners/SaveButtonId/FetchAsPost/afterFetch/Status200.js
@@ -1,15 +1,4 @@
 const StartFunc = (data) => {
-    let localAddondata = data.JsonData
-    jFLocalToInputAddonsId(localAddondata)
-};
-
-let jFLocalToInputAddonsId = (inValue) => {
-    let jVarLocalHtmlId = 'AddonsId';
-    let jVarLocalAddonsId = document.getElementById(jVarLocalHtmlId);
-
-    if (jVarLocalAddonsId === null === false) {
-        jVarLocalAddonsId.innerHTML = inValue;
-    };
 };
 
 export { StartFunc };
