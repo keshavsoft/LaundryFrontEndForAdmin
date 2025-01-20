cb7a2ef02fcd3e80136a28bb0b4269edbbf6b86e
sharath0036 <ksharathbabu2@gmail.com>
2025-01-20 13:56:38 +0530

    ui for all dc

---
+++ src/AdminAllDays/CommonConfig/Dcs/KCode/generateVariables.js
@@ -0,0 +1,21 @@
+import { StartFunc as prepareSideBar } from "./prepareSideBar.js";
+
+const StartFunc = ({ mode, inFilesArray }) => {
+    const variables = {};
+    let LocalFiles = inFilesArray;
+    const sidebarItems = prepareSideBar();
+
+    Object.keys(LocalFiles).forEach((filename) => {
+        variables[filename + '.html'] = {
+            web_title: "Mazer Admin Dashboard",
+            filename: filename + '.html',
+            sidebarItems,
+            isDev: mode === 'development',
+            title:filename
+        };
+    });
+
+    return variables;
+};
+
+export { StartFunc };
