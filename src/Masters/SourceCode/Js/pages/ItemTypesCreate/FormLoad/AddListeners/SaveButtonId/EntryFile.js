efea709b48846b65fc960d5641b2a679b18af10e
keshavsoft <nknnkumar@yahoo.com>
2025-01-20 09:55:31 +0530

    FetchAsPost started in admin masters

--- src/Masters/SourceCode/Js/pages/ItemTypesCreate/FormLoad/AddListeners/SaveButtonId/EntryFile.js
+++ src/Masters/SourceCode/Js/pages/ItemTypesCreate/FormLoad/AddListeners/SaveButtonId/EntryFile.js
@@ -1,10 +1,11 @@
-import { StartFunc as StartFuncFuncToRun } from "./FetchFromFuncs/Entry.js";
+// import { StartFunc as StartFuncFuncToRun } from "./FetchFromFuncs/Entry.js";
+import { StartFunc as StartFuncFetchAsPost } from "./FetchAsPost/entryFile.js";
 
 let StartFunc = () => {
     let jVarLocalRefreshBSTableId = document.getElementById("SaveButtonId");
 
     if (jVarLocalRefreshBSTableId === null === false) {
-        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
+        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFetchAsPost);
     };
 };
 
