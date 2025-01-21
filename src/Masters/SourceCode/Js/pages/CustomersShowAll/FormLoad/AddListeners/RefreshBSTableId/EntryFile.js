211cd177bbacfce8ae60e4c998c3f24ee0495376
keshavsoft <nknnkumar@yahoo.com>
2025-01-21 10:49:45 +0530

    admin masters customers show all

--- src/Masters/SourceCode/Js/pages/CustomersShowAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/Masters/SourceCode/Js/pages/CustomersShowAll/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,10 +1,11 @@
-import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
+// import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
+import { StartFunc as StartFuncFetchAsGet } from "./FetchAsGet/entryFile.js";
 
 let StartFunc = () => {
     let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
 
     if (jVarLocalRefreshBSTableId === null === false) {
-        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
+        jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFetchAsGet);
     };
 };
 
