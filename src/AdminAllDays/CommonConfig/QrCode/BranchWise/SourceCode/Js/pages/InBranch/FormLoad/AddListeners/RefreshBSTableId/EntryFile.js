e64355e0bd3d6fb8b26560d3274c58a3f57211a8
sharath0036 <ksharathbabu2@gmail.com>
2025-01-21 12:23:27 +0530

    js structure

--- src/AdminAllDays/CommonConfig/QrCode/BranchWise/SourceCode/Js/pages/InBranch/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
+++ src/AdminAllDays/CommonConfig/QrCode/BranchWise/SourceCode/Js/pages/InBranch/FormLoad/AddListeners/RefreshBSTableId/EntryFile.js
@@ -1,17 +1,12 @@
-import { StartFunc as StartFuncFuncToRun } from "./ButtonClickFunc.js";
+import { StartFunc as StartFuncFuncToRun } from "./FetchAsGet/entryFile.js";
 
 let StartFunc = () => {
-    // let jVarLocalRefreshBSTableId = document.getElementById("RefreshBSTableId");
-
     const arrClass = document.querySelectorAll(".RefreshBSTableClass");
 
     for (let i of arrClass) {
         i.addEventListener("click", StartFuncFuncToRun);
     };
-
-    // if (jVarLocalRefreshBSTableId === null === false) {
-    //     jVarLocalRefreshBSTableId.addEventListener("click", StartFuncFuncToRun);
-    // };
+   
 };
 
 export { StartFunc };
