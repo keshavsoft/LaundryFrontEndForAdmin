a404b602e79bac71b780031caea88dd2c3b8bd44
keshavsoft <nknnkumar@yahoo.com>
2025-03-08 16:58:34 +0530

    entryfile case

--- src/Masters/SourceCode/Js/pages/AddOnsDelete/FormLoad/BuildBsTable/EntryFile.js
+++ src/Masters/SourceCode/Js/pages/AddOnsDelete/FormLoad/BuildBsTable/EntryFile.js
@@ -1,4 +1,5 @@
 import optionsJson from './options.json' with {type: 'json'};
+
 import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
 import { StartFunc as StartFuncForColumns } from "./ForColumns/EntryFile.js";
 
