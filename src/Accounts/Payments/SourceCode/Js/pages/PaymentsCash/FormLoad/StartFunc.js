b78d1d78fcdc5280985a26b951b3e9abbb6875e4
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-03-12 12:53:48 +0530

    Branch Name

--- src/Accounts/Payments/SourceCode/Js/pages/PaymentsCash/FormLoad/StartFunc.js
+++ src/Accounts/Payments/SourceCode/Js/pages/PaymentsCash/FormLoad/StartFunc.js
@@ -1,11 +1,13 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.js";
 import { StartFunc as StartFuncFromUrlParams } from "./FromUrlParams/EntryFile.js";
+import { StartFunc as ToDataList } from "./ToDataList/StartFunc.js";
 
 const StartFunc = () => {
     StartFuncFromUrlParams();
     StartFuncBuildBsTable();
     StartFuncAddListeners();
+    ToDataList();
 };
 
 export { StartFunc };
--- src/Accounts/Payments/SourceCode/Js/pages/PaymentsGSTShowAll/FormLoad/StartFunc.js
+++ src/Accounts/Payments/SourceCode/Js/pages/PaymentsGSTShowAll/FormLoad/StartFunc.js
@@ -1,6 +1,7 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.js";
 import { StartFunc as StartFuncFromUrlParams } from "./FromUrlParams/EntryFile.js";
+import { StartFunc as ToDataList } from "./ToDataList/StartFunc.js";
 
 const StartFunc = () => {
     StartFuncFromUrlParams();
@@ -8,6 +9,7 @@ const StartFunc = () => {
     StartFuncAddListeners();
     jFLocalToDateInputId();
     jFLocalFromDateInputId();
+    ToDataList();
   
 };;
 let jFLocalToDateInputId = () => {
